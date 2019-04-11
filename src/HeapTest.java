10355c3b1839a9ca54a6d3832ca904fe389a8c7b
tsinghqs <tsingh2324@gmail.com>
2019-04-11 18:20:31 -0400

    update tests

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -60,4 +60,24 @@ public class HeapTest extends TestCase {
         
     }
     
+    /**
+     * Test record methods
+     */
+    public void testRecord()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            check[i] = (byte)i;
+        }
+        Record top_rec = new Record(check);
+        Record shift = new Record(check);
+        top_rec.moveTo(shift);
+        shift.copyContentTo(top_rec);
+        top_rec.swapContentWith(shift);
+        double val = shift.getValue();
+        String tostring = top_rec.toString();
+        assertEquals(val, 0.0, 1.0);
+        
+    }
+    
 }
